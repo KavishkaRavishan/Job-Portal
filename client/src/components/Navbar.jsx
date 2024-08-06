a923d06af199a2748bd0a3ed2ef7b499fd4b47f0
Kavishka <kkravishan2@gmail.com>
2024-08-06 21:08:35 +0530

    nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,17 +16,17 @@ const Navbar = () => {
   return (
     <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4 ">
       <nav className="flex justify-between items-center py-6">
-        {/*icon*/}
+        {/* Icon */}
         <img
           src="https://images.app.goo.gl/VrFxPEMgHwpAYiA5A"
+          alt="Job Portal Icon"
           width="60"
           height="60"
-          viewBox="0 0 60 60"
         />
-        {/*nav items */}
-        <ul className=" flex gap-12 ">
+        {/* Nav items */}
+        <ul className="flex gap-12">
           {navItems.map(({ path, title }) => (
-            <li key={path} className="text-base text-primary ">
+            <li key={path} className="text-base text-primary">
               <NavLink
                 to={path}
                 className={({ isActive }) => (isActive ? "active" : "")}
@@ -36,14 +36,14 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        {/*sign up and login button*/}
-        <div className="text-semibold text-primary  font-medium space-x-5 hidden lg:block">
+        {/* Sign up and login buttons */}
+        <div className="text-semibold text-primary font-medium space-x-5 hidden lg:block">
           <Link to="/login" className="py-2 px-5 border rounded">
             Log in
           </Link>
           <Link
-            to="/login"
-            className="py-2 px-5 border rounded bg-green-500 text-white "
+            to="/signup"
+            className="py-2 px-5 border rounded bg-green-500 text-white"
           >
             Sign up
           </Link>
