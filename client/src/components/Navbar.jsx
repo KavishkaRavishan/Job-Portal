65aa52a2bd7d4110fa1a1ec16b1a86416ed9788a
Kavishka <kkravishan2@gmail.com>
2024-08-06 21:27:54 +0530

    respon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,19 +14,49 @@ const Navbar = () => {
   ];
 
   return (
-    <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4 ">
+    <header className="max-w-screen-2xl container mx-auto xl:px-24 px-4">
       <nav className="flex justify-between items-center py-6">
-        {/*icon*/}
+        {/* Icon */}
         <img
-          src="\images\jobnav.png"
+          src="/images/jobnav.png"
+          alt="Job Portal Icon"
           width="60"
           height="60"
-          viewBox="0 0 60 60"
         />
-        {/*nav items */}
-        <ul className=" flex gap-12 ">
+
+        {/* Hamburger Icon */}
+        <div className="lg:hidden">
+          <button
+            onClick={handleMenuToggler}
+            className="text-gray-500 hover:text-gray-700 focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={
+                  isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"
+                }
+              />
+            </svg>
+          </button>
+        </div>
+
+        {/* Nav items */}
+        <ul
+          className={`lg:flex gap-12 ${
+            isMenuOpen ? "block" : "hidden"
+          } lg:block`}
+        >
           {navItems.map(({ path, title }) => (
-            <li key={path} className="text-base text-primary ">
+            <li key={path} className="text-base text-primary">
               <NavLink
                 to={path}
                 className={({ isActive }) => (isActive ? "active" : "")}
@@ -36,19 +66,37 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        {/*sign up and login button*/}
-        <div className="text-semibold text-primary  font-medium space-x-5 hidden lg:block">
+
+        {/* Sign up and login buttons */}
+        <div className="text-semibold text-primary font-medium space-x-5 hidden lg:block">
           <Link to="/login" className="py-2 px-5 border rounded">
             Log in
           </Link>
           <Link
-            to="/login"
-            className="py-2 px-5 border rounded bg-green-500 text-white "
+            to="/signup"
+            className="py-2 px-5 border rounded bg-green-500 text-white"
           >
             Sign up
           </Link>
         </div>
       </nav>
+
+      {/* Mobile sign up and login buttons */}
+      <div
+        className={`lg:hidden ${
+          isMenuOpen ? "block" : "hidden"
+        } space-y-4 mt-4`}
+      >
+        <Link to="/login" className="py-2 px-5 border rounded block">
+          Log in
+        </Link>
+        <Link
+          to="/signup"
+          className="py-2 px-5 border rounded bg-green-500 text-white block"
+        >
+          Sign up
+        </Link>
+      </div>
     </header>
   );
 };
